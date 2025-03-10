cc16c67590bdd3dbfbb0f0ef2e60354543cad3b1
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-03-10 13:01:16 +0000

    created order migration

---
+++ database/migrations/2025_03_10_125524_create_order_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('users_id');
+            $table->float('total_price');
+            $table->foreign('users_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order');
+    }
+};
