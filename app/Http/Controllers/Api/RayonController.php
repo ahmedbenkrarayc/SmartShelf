3f274a5cb38e068a1ea93111a99727cf0252ebe6
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-03-11 21:51:54 +0000

    created rayon controller with crud

---
+++ app/Http/Controllers/Api/RayonController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Rayon;
+use App\Http\Resources\RayonResource;
+use App\Http\Requests\StoreRayonRequest;
+use App\Http\Requests\UpdateRayonRequest;
+
+class RayonController extends Controller
+{
+    public function index()
+    {
+        $rayons = Rayon::all();
+        return RayonResource::collection($rayons);
+    }
+
+
+    public function store(StoreRayonRequest $request)
+    {
+        $request->validated();
+
+        $rayon = Rayon::create([
+            'name' => $request->name,
+            'description' => $request->description,
+        ]);
+
+        return new RayonResource($rayon);
+    }
+
+
+    public function show(int $id)
+    {
+        $rayon = Rayon::find($id);
+        if(!$rayon){
+            return response()->json([
+                'message' => 'Rayon not found !'
+            ], 404);
+        }
+
+        return new RayonResource($rayon);
+    }
+
+
+    public function update(UpdateRayonRequest $request, int $id)
+    {
+        $validated = $request->validated();
+        $rayon = Rayon::find($id);
+        if(!$rayon){
+            return response()->json([
+                'message' => 'Rayon not found !'
+            ], 404);
+        }
+
+        $rayon->update($validated);
+        return new RayonResource($rayon);
+    }
+
+
+    public function destroy(int $id)
+    {
+        $rayon = Rayon::find($id);
+        if(!$rayon){
+            return response()->json([
+                'message' => 'Rayon not found !'
+            ], 404);
+        }
+
+        $rayon->delete();
+        return response()->json(['message' => 'Rayon deleted successfully'], 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\RayonController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -19,10 +20,16 @@ Route::middleware(['auth:sanctum', 'auth.role:client'])->group(function(){
     });
 });
 
-Route::get('/category/list', [CategoryController::class, 'index']);
-Route::post('/category/create', [CategoryController::class, 'store']);
-Route::put('/category/update/{id}', [CategoryController::class, 'update']);
-Route::get('/category/{id}', [CategoryController::class, 'show']);
-Route::delete('/category/{id}', [CategoryController::class, 'destroy']);
+Route::get('/rayon/list', [RayonController::class, 'index']);
+Route::post('/rayon/create', [RayonController::class, 'store']);
+Route::put('/rayon/update/{id}', [RayonController::class, 'update']);
+Route::get('/rayon/{id}', [RayonController::class, 'show']);
+Route::delete('/rayon/{id}', [RayonController::class, 'destroy']);
+
 Route::prefix('category')->middleware(['auth:sanctum', 'auth.role:admin'])->group(function(){
+    Route::get('/list', [CategoryController::class, 'index']);
+    Route::post('/create', [CategoryController::class, 'store']);
+    Route::put('/update/{id}', [CategoryController::class, 'update']);
+    Route::get('/{id}', [CategoryController::class, 'show']);
+    Route::delete('/{id}', [CategoryController::class, 'destroy']);
 });
