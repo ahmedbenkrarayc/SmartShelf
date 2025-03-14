541b90295e858ee5ae78e697286f4c4f3799bd52
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-03-14 12:12:00 +0000

    fixed update produit request (product table from product to produit)

--- app/Http/Requests/UpdateProduitRequest.php
+++ app/Http/Requests/UpdateProduitRequest.php
@@ -11,7 +11,7 @@ class UpdateProduitRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class UpdateProduitRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255', 'unique:produit,name'.$this->route('id')], 
+            'name' => ['required', 'string', 'max:255', 'unique:produit,name,'.$this->route('id')], 
             'description' => ['required', 'string', 'max:255'], 
             'price' => ['required', 'numeric', 'min:0'], 
             'stock' => ['required', 'integer', 'min:0'], 
